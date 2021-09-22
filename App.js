0552c4f0291e702b300daabe7d67924e86727a06
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-22 13:10:33 +0530

    welcome screen

--- App.js
+++ App.js
@@ -1,34 +1,8 @@
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>You clicked the button {counter} times</Text>
-      <Button color="teal" disabled={counter == 10} title="Click Me" onPress={() => setCounter(counter + 1)} />
-      <StatusBar style="auto" />
-    </View>
-  );
+    <WelcomeScreen />
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#101010',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontSize: 20,
-    textAlign: 'center',
-  },
-  button: {
-    backgroundColor: 'teal',
-    color: '#fff',
-    marginTop: 5,
-  }
-});
