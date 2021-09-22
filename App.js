ce5f39a6056f51345f5e55e1c0655ba8a8250dca
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-22 13:21:05 +0530

    view image screen

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <WelcomeScreen />
+    <ViewImageScreen />
   )
 }
