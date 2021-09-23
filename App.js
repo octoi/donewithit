20b510eea85a7c6ff7131b031f32a712358953a8
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-23 17:20:01 +0530

    utilities

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { View } from 'react-native';
+
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 export default function App() {
-  return (
-    <ViewImageScreen />
-  )
+  return <ViewImageScreen />;
 }
