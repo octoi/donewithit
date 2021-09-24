758b9759d3e4f06dc068231ba2930b4eee807113
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-24 07:02:39 +0530

    utils

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
+import LoginScreen from './app/screens/LoginScreen';
 
 export default function App() {
-  return <MessagesScreen />;
+  return <LoginScreen />;
 }
---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Image, StyleSheet } from 'react-native';
+import * as Yup from 'yup';
+
+import AppFormField from '../components/AppFormField';
+import Screen from '../components/Screen';
+import SubmitButton from '../components/SubmitButton';
+import AppForm from '../components/AppForm';
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label('Email'),
+  password: Yup.string().required().min(4).label('Password'),
+});
+
+export default function LoginScreen() {
+  return (
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require('../assets/logo-red.png')} />
+      <AppForm
+        initialValues={{ email: '', password: '' }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField
+          autoCapitalize='none'
+          autoCorrect={false}
+          icon='email'
+          keyboardType='email-address'
+          name='email'
+          placeholder='Email'
+          textContentType='emailAddress'
+        />
+        <AppFormField
+          autoCapitalize='none'
+          autoCorrect={false}
+          icon='lock'
+          name='password'
+          placeholder='Password'
+          secureTextEntry
+          textContentType='password'
+        />
+        <SubmitButton title='Login' />
+      </AppForm>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: 'center',
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
